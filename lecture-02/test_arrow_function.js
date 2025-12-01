a9097405abed53cb2769303eb100c7446d597651
Alif024 <aleefrock12345@gmail.com>
2025-12-01 16:53:53 +0700

    #

--- lecture-02/test_arrow_function.js
+++ lecture-02/test_arrow_function.js
@@ -2,4 +2,10 @@ const a = 5;
 const b = 10;
 
 const add = (a, b) => a + b;
+
+const xadd = (a, b) => {
+  return a + b;
+};
+
 console.log(add(a, b)); // Output: 15
+console.log(xadd(a, b)); // Output: 15
